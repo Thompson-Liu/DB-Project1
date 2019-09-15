03032cf4d4f53456c9d28e94a8b1860f624afc8e
Alicia <yy354@cornell.edu>
2019-09-15 17:24:38 -0400

    join finished

--- DBPrac/src/operator/JoinOperator.java
+++ DBPrac/src/operator/JoinOperator.java
@@ -15,43 +15,29 @@ import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.schema.Table;
 import net.sf.jsqlparser.statement.select.Join;
 import net.sf.jsqlparser.statement.select.PlainSelect;
-import parser.EvaluateExpression;
 import parser.EvaluateWhere;
 
 public class JoinOperator extends Operator {
 
-	private DataTable joinResultTable;
 	private Operator leftOperator;
 	private Operator rightOperator;
-	private DataTable leftTable;
-	private DataTable rightTable;
-//	private ArrayList<Table> joinTables;
-//	private ArrayList<String> leftTables;	//deep left join: outer table, composed of joined result sofar
-//	private String rightTable; 				//inner table to be joined
 	private Expression joinExp;
 
 	public JoinOperator(Operator LeftOperator, Operator RightOperator, Expression expression) {
 		this.joinExp=expression;
 		this.leftOperator = LeftOperator;
 		this.rightOperator = RightOperator;
-		this.leftTable = LeftOperator.dump();
-		this.rightTable = RightOperator.dump();
-//		super(rightTable,expression);
-//		joinExp = expression;
-//		catalog = Catalog.getInstance();
-//		this.leftTables = leftTables;
-//		this.rightTable = rightTable;
 	}
 	
 	//  could use scan / or could also use right table directly as input
-	public Tuple getNextTuple() {
+	public Tuple getNextTuple(DataTable leftTable, DataTable rightTable) {
 		Tuple next;
 		Tuple left;
 		Tuple right;
 		EvaluateWhere evawhere = new EvaluateWhere(joinExp );
 		while((left=leftOperator.getNextTuple())!=null) {
 			while((right=rightOperator.getNextTuple())!=null) {
-				if((next=evawhere.evaluate(left,right,leftTable, rightTable))!=null) {
+				if((next=evawhere.evaluate(left,right,leftTable.getSchema(), rightTable.getSchema()))!=null) {
 					return next;
 				}
 			}
@@ -59,19 +45,17 @@ public class JoinOperator extends Operator {
 		}
 		return null;
 	}
-	
-	public DataTable dump() {
 		
-		DataTable data = new DataTable("output");
-		for(int i=0; i<outerTable.cardinality();i++) {
-			Tuple left= new Tuple(outerTable.getData(i));
-			Tuple right;
-			while((right= this.getNextTuple())!=null) {
-				data.addData(right.getTuple());
-			}
-			super.reset();
+	public DataTable dump(DataTable leftTable, DataTable rightTable) {
+		ArrayList<String> ret= (ArrayList<String>) leftTable.getSchema().clone();
+		ret.addAll(rightTable.getSchema());
+		DataTable result = new DataTable("Output", ret);
+		Tuple tup = new Tuple();
+		while ((tup = getNextTuple(leftTable, rightTable)) != null) {
+			result.addData(tup.getTuple());
 		}
-		return data;
+		
+		return result;
 	}
 
 }
