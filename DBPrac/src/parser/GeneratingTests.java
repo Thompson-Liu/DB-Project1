952cc1a447001813874df4da85f73fd6f973812d
Alicia <yy354@cornell.edu>
2019-09-22 20:04:46 -0400

    natureal number to 1000

--- DBPrac/src/parser/GeneratingTests.java
+++ DBPrac/src/parser/GeneratingTests.java
@@ -4,8 +4,8 @@ import java.io.*;
 
 public class GeneratingTests {
 	
-	static String fileName ="Events";
-	static int row= 30;
+	static String fileName ="Natural";
+	static int row= 1000;
 	static int column = 3;
 	
 	public static void main(String[] arg0) {
@@ -17,28 +17,31 @@ public class GeneratingTests {
 			int countTwo=0;
 			while(count<row)
 		      {
-		        	int a=rand.nextInt(10);
+//		        	int a=rand.nextInt(10);
+				int a = count;
 		 			out.print(a);
 		 			out.print(",");
 		 			
-		            int b=rand.nextInt(10);
+		 			int b=count;
+//		            int b=rand.nextInt(10);
 		            out.print(b);
 		            out.print(",");
 		            
-		            int c=rand.nextInt(10);
+		            int c=count;
+//		            int c=rand.nextInt(10);
 		 			out.print(c);
-		 			out.print(",");
-		 			
-		 			int d=rand.nextInt(10);
-		 			out.print(d);
-		 			out.print(",");
-		 			
-		 			int e=rand.nextInt(10);
-		 			out.print(e);
-		 			out.print(",");
-		 			
-		 			int f=1;
-		 			out.print(f);
+//		 			out.print(",");
+//		 			
+//		 			int d=rand.nextInt(10);
+//		 			out.print(d);
+//		 			out.print(",");
+//		 			
+//		 			int e=rand.nextInt(10);
+//		 			out.print(e);
+//		 			out.print(",");
+//		 			
+//		 			int f=1;
+//		 			out.print(f);
 		 			
 		 			
 		            count++;
