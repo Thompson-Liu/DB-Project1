8188f7288b9149aa321df3d50895fc16c4cbaa48
Ziwei Gu <zg48@cornell.edu>
2019-09-24 21:24:02 -0400

    Add more docs

--- DBPrac/src/operator/JoinOperator.java
+++ DBPrac/src/operator/JoinOperator.java
@@ -3,7 +3,6 @@
  */
 package operator;
 
-import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,6 +12,7 @@ import dataStructure.Tuple;
 import net.sf.jsqlparser.expression.Expression;
 import parser.EvaluateWhere;
 
+/** the class for the join operator */
 public class JoinOperator extends Operator {
 
 	private DataTable currentTable;
@@ -21,63 +21,56 @@ public class JoinOperator extends Operator {
 	private Expression joinExp;
 	private boolean resetFlag= true;
 	private Tuple left;
-	private HashMap<String,String> tableAlias;
+	private HashMap<String, String> tableAlias;
 
 	/** Constructor to create JOIN operator
 	 * 
-	 * @param LeftOperator  	the left operator of join
-	 * @param RightOperator     the right operator of join
-	 * @param expression  		the where expression to select tuple
-	 * @param tableAlias  		hashmap of <tablename,alias>
-	 */
-	public JoinOperator(Operator LeftOperator, Operator RightOperator, 
-			Expression expression,HashMap<String,String> tableAlias) {
+	 * @param LeftOperator the left operator of join
+	 * @param RightOperator the right operator of join
+	 * @param expression the where expression to select tuple
+	 * @param tableAlias hashmap of <tablename,alias> */
+	public JoinOperator(Operator LeftOperator, Operator RightOperator,
+		Expression expression, HashMap<String, String> tableAlias) {
 		joinExp= expression;
 		ArrayList<String> cur= (ArrayList<String>) LeftOperator.schema().clone();
 		cur.addAll(RightOperator.schema());
 		currentTable= new DataTable(LeftOperator.getTableName() + " " + RightOperator.getTableName(), cur);
 		leftOperator= LeftOperator;
 		rightOperator= RightOperator;
-		this.tableAlias=tableAlias;
+		this.tableAlias= tableAlias;
 	}
 
-	/**
-	 * reset both left operator and right operator to start from beginning
-	 */
+	/** reset both left operator and right operator to start from beginning */
 	@Override
 	public void reset() {
 		leftOperator.reset();
 		rightOperator.reset();
 	}
 
-	/**
-	 * @return the table name from where the operator reads the data
-	 */
+	/** @return the table name from where the operator reads the data */
 	@Override
 	public String getTableName() {
 		return currentTable.getTableName();
 	}
 
-	/**
-	 * @return the schema of the data table that is read by the operator
-	 */
+	/** @return the schema of the data table that is read by the operator */
 	@Override
 	// return the schema of the current table
 	public ArrayList<String> schema() {
 		return currentTable.getSchema();
 	}
 
-	/** for every left tuple loop through every right tuple, until finding a valid tuple or no more tuple to add
-	 * reset to next left tuple when one tuple is done permutating right table
-	 * @return Returns the next tuple read from the data
-	 */
+	/** for every left tuple loop through every right tuple, until finding a valid tuple or no more
+	 * tuple to add reset to next left tuple when one tuple is done permutating right table
+	 * 
+	 * @return Returns the next tuple read from the data */
 	@Override
 	public Tuple getNextTuple() {
 		Tuple next= null;
 		boolean flag= true;
 		Tuple right;
 		EvaluateWhere evawhere= new EvaluateWhere(joinExp, leftOperator.schema(),
-			rightOperator.schema(),tableAlias);
+			rightOperator.schema(), tableAlias);
 
 		while (flag) {
 			if (resetFlag) {
@@ -106,23 +99,21 @@ public class JoinOperator extends Operator {
 
 	}
 
-	/**
-	 * Prints the data read by operator to the PrintStream [ps]
+	/** Prints the data read by operator to the PrintStream [ps]
 	 * 
-	 * @param ps      The print stream that the output will be printed to
-	 * @param print   boolean decides whether the data will actually be printed 
-	 */
+	 * @param ps The print stream that the output will be printed to
+	 * @param print boolean decides whether the data will actually be printed */
 	@Override
 	public void dump(PrintStream ps, boolean print) {
 		Tuple next;
 		while ((next= getNextTuple()) != null) {
 		}
-		if (print) { currentTable.printTable(ps); }
+		if (print) {
+			currentTable.printTable(ps);
+		}
 	}
 
-	/** 
-	 * @return the data read by the operator in DataTable data structure
-	 */
+	/** @return the data read by the operator in DataTable data structure */
 	@Override
 	public DataTable getData() {
 		dump(System.out, false);
--- DBPrac/src/operator/SortOperator.java
+++ DBPrac/src/operator/SortOperator.java
@@ -7,6 +7,7 @@ import java.util.List;
 import dataStructure.DataTable;
 import dataStructure.Tuple;
 
+/** the class for the sort operator that sorts the data another operator generates. */
 public class SortOperator extends Operator {
 
 	private DataTable buffer;
