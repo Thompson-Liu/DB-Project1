767a3bef980a40821eaef11df45b66ee449a4695
Ziwei Gu <zg48@cornell.edu>
2019-10-05 19:21:01 -0400

    change

--- DBPrac/src/Operators/PhysicalPlanBuilder.java
+++ DBPrac/src/Operators/PhysicalPlanBuilder.java
@@ -23,29 +23,30 @@ public class PhysicalPlanBuilder {
 
 	public static Operator generatePlan(LogicalOperator lop) {
 		if (lop == null) { return null; }
+		Operator root;
 		if (lop instanceof ScanLogOp) {
-			ScanOperator root= new ScanOperator(((ScanLogOp) lop).getTableName(), ((ScanLogOp) lop).getAliasName());
+			root= new ScanOperator(((ScanLogOp) lop).getTableName(), ((ScanLogOp) lop).getAliasName());
 		} else if (lop instanceof SelectLogOp) {
-			SelectOperator root= new SelectOperator(((SelectLogOp) lop).getSelectExpr(),
+			root= new SelectOperator(((SelectLogOp) lop).getSelectExpr(),
 				generatePlan(((SelectLogOp) lop).getChildren()[0]), ((SelectLogOp) lop).getAlias());
 		} else if (lop instanceof ProjectLogOp) {
-			ProjectOperator root= new ProjectOperator(generatePlan(((ProjectLogOp) lop).getChildren()[0]),
+			root= new ProjectOperator(generatePlan(((ProjectLogOp) lop).getChildren()[0]),
 				((ProjectLogOp) lop).getItems(), ((ProjectLogOp) lop).getAlias());
 		} else if (lop instanceof JoinLogOp) {
-			JoinOperator root= new JoinOperator(generatePlan(((JoinLogOp) lop).getLeftChild()),
+			root= new JoinOperator(generatePlan(((JoinLogOp) lop).getLeftChild()),
 				generatePlan(((JoinLogOp) lop).getRightChild()), ((JoinLogOp) lop).getJoinExpression(),
 				((JoinLogOp) lop).getAlias());
 		} else if (lop instanceof SortLogOp) {
-			SortOperator root= new SortOperator(generatePlan(((SortLogOp) lop).getChildren()[0]),
+			root= new SortOperator(generatePlan(((SortLogOp) lop).getChildren()[0]),
 				((SortLogOp) lop).getColumns());
-		} else if (lop instanceof DuplicateEliminationLogOp) {
-			DuplicateEliminationOperator root= new DuplicateEliminationOperator(
+		} else {
+			root= new DuplicateEliminationOperator(
 				(SortOperator) generatePlan(((DuplicateEliminationLogOp) lop).getChidren()[0]));
 		}
-		for (LogicalOperator child : lop.getChildren()) {
-
-		}
-		return null;
+//		for (LogicalOperator child : lop.getChildren()) {
+//
+//		}
+		return root;
 
 	}
 
@@ -53,9 +54,9 @@ public class PhysicalPlanBuilder {
 		return new ScanOperator(scanLop.getTableName(), scanLop.getAliasName());
 	}
 
-	public Operator visit(SelectLogOp selectLop) {
-		return new SelectOperator(selectLop.getSelectExpr(), visit(selectLop.getChildren()[0]), selectLop.getAlias());
-	}
+//	public Operator visit(SelectLogOp selectLop) {
+//		return new SelectOperator(selectLop.getSelectExpr(), visit(selectLop.getChildren()[0]), selectLop.getAlias());
+//	}
 
 //	public Operator visit(ProjectLogOp projectLop) {
 //		return new ProjectOperator();
