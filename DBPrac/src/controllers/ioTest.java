e5ea42b72f6ccefeaf080059c6d63eb5c45948b1
Ziwei Gu <zg48@cornell.edu>
2019-10-06 13:58:59 -0400

    improve read

--- DBPrac/src/controllers/ioTest.java
+++ DBPrac/src/controllers/ioTest.java
@@ -1,14 +1,13 @@
 package controllers;
 
 import fileIO.BinaryTupleReader;
-import fileIO.BinaryTupleWriter;
 
 public class ioTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BinaryTupleReader tr= new BinaryTupleReader(
-			"/Users/yutingyang/Desktop/db_prac/DB-Project2/DBPrac/samples/input/db/data/Boats");
+			"/Users/ziweigu/Desktop/DB-Project1/DBPrac/samples/input/db/data/Boats");
 		System.out.println(tr.readData());
 //		System.out.println(tr.readData().get(2).printData());
 //
@@ -16,9 +15,9 @@ public class ioTest {
 //		tw.writeTuple(tr.readData());
 
 //		System.out.println(tr.readData().get(2).printData());
-		BinaryTupleWriter tw= new BinaryTupleWriter("result");
-		tw.writeTable(tr.readData());
-		tw.dump();
+//		BinaryTupleWriter tw= new BinaryTupleWriter("result");
+//		tw.writeTable(tr.readData());
+//		tw.dump();
 	}
 
 }
