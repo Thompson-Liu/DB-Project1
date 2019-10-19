971e68348e721410bbda124e48ca12ad50cb0b3b
yutingyang yishuzhang <yy354@cornell.edu>
2019-10-19 09:56:33 -0400

    finished external sort

--- DBPrac/src/fileIO/ReadableTupleWriter.java
+++ DBPrac/src/fileIO/ReadableTupleWriter.java
@@ -12,34 +12,64 @@ import java.util.ArrayList;
 import dataStructure.Tuple;
 
 public class ReadableTupleWriter implements TupleWriter{
-	
+
 	private ArrayList<ArrayList<Integer>> data;
 	private BufferedOutputStream buffer;
 	private FileOutputStream fout;
 
-	
+
 	public ReadableTupleWriter(String name) {
-			try {
-				fout= new FileOutputStream(name);
-				data= new ArrayList<ArrayList<Integer>>();
-				buffer = new BufferedOutputStream(fout);
-			} catch (Exception e) {
-				System.err.print(" Readable Tuple Write initialize fail. ");
-				e.printStackTrace();
-			}
+		try {
+			fout= new FileOutputStream(name);
+			data= new ArrayList<ArrayList<Integer>>();
+			buffer = new BufferedOutputStream(fout);
+		} catch (Exception e) {
+			System.err.print(" Readable Tuple Write initialize fail. ");
+			e.printStackTrace();
+		}
 	}
-	
+
 	public void addTable(ArrayList<Tuple> dataTable) {
 		for (Tuple tup : dataTable) {
 			ArrayList<Integer> a = tup.getTuple();
 			data.add(a);
 		}
 	}
-	
-	
-	public void write() {
+
+
+	@Override
+	public void close() {
 		try {
-			
+			//			fout.close();
+			buffer.close();
+		} catch (IOException e) {
+			System.err.println("Fail to close Readable Tuple Writer. ");
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void write(ArrayList<Tuple> data) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addNextTuple(Tuple tup) {
+		data.add(tup.getTuple());
+	}
+
+	@Override
+	public void dump() {
+		try {
+
 			for (ArrayList<Integer> x : data) {
 				for (int i= 0; i < x.size() - 1; i++) {
 					String out = x.get(i) + ",";
@@ -50,34 +80,11 @@ public class ReadableTupleWriter implements TupleWriter{
 				String newLine = "\n";
 				buffer.write(newLine.getBytes());
 			}
-			
+
 		} catch (IOException e) {
 			System.err.print("BinaryTupleWriter dump fails: " + e);
 			e.printStackTrace();
 		}
-	}
 
-	@Override
-	public void close() {
-		try {
-//			fout.close();
-			buffer.close();
-		} catch (IOException e) {
-			System.err.println("Fail to close Readable Tuple Writer. ");
-			e.printStackTrace();
-		}
-		
-	}
-
-	@Override
-	public void write(ArrayList<Tuple> data) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void reset() {
-		// TODO Auto-generated method stub
-		
 	}
 }
