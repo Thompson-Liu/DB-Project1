1751765532364e784cb6038ffc2462629be42d27
Ziwei Gu <zg48@cornell.edu>
2019-10-27 10:47:50 -0400

    work on index scan

--- DBPrac/src/bpTree/Deserializer.java
+++ DBPrac/src/bpTree/Deserializer.java
@@ -1,7 +1,6 @@
 package bpTree;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
@@ -11,6 +10,9 @@ public class Deserializer {
 	private FileInputStream fin;
 	private FileChannel fc;
 	private ByteBuffer buffer;
+	private int rootInd;
+	private int numLeaves;
+	private int startLeaf;
 
 	/** The deserializer deserializes a serialized B+-tree, which is stored in an index file.
 	 * 
@@ -22,26 +24,32 @@ public class Deserializer {
 			fin= new FileInputStream(indexFile);
 			fc= fin.getChannel();
 			buffer= ByteBuffer.allocate(4096);
-
-		} catch (FileNotFoundException e) {
+			fc.read(buffer);
+			rootInd= buffer.getInt(0); // first int on the header page is the address of the root
+			numLeaves= buffer.getInt(4); // # of leaves in the tree
+			startLeaf= 0;
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
 
-	/** Find the rid of the first key that matches the scan condition */
+	/** Find the rid of the first key that falls within the range of the scan.
+	 * 
+	 * @param lowKey: the lower bound of the range of the scan (inclusive)
+	 * @param highKey: the higher bound of the range of the scan (inclusive) */
 	public int[] getRid(int lowKey, int highKey) {
-		int loc= initialDescent(lowKey);
+		startLeaf= initialDescent(lowKey);
 		try {
 			buffer.clear();
-			fc.position(loc * 4096);
+			fc.position(startLeaf * 4096);
 			fc.read(buffer);
 			int flag= buffer.getInt(0);
-			if (flag == 1) return null; // non-leaf nodes
+			assert (flag == 0); // must be leaf nodes
 			int numElements= buffer.getInt(4);
 			int pos= 8; // starting pos
-			// A data entry: k, # of rids, (p,t) for each rid
+			// A data entry contains: k, # of rids, (p,t) for each rid
 			for (int i= 0; i < numElements; i++ ) {
 				int key= buffer.getInt(pos);
 				int numRids= buffer.getInt(pos + 4);
@@ -58,36 +66,32 @@ public class Deserializer {
 		return null;
 	}
 
-	/** Performs a DFS to find the location of low key. */
+	/** Performs a root-to-leaf descent to find the location of the first key that's greater than low
+	 * key. */
 	private int initialDescent(int lowKey) {
 		try {
-			buffer.clear();
-			fc.read(buffer);
-			int rootInd= buffer.getInt(0); // first int on the header page is the address of the root
-			int numLeaves= buffer.getInt(4); // # of leaves in the tree
-			int order= buffer.getInt(8); // order of the tree
-
-			int startInd= rootInd;
+			int startInd= rootInd; // Start from root
 			while (true) { // iterative depth-first search
-				buffer.clear();
-				fc.position(startInd * 4096);
+				buffer.clear(); // clear existing buffer
+				fc.position(startInd * 4096); // set file channel
 				fc.read(buffer);
 				int flag= buffer.getInt(0); // 1: index node; 0: leaf node
-				if (flag == 0) return startInd; // leaf node
-				int numKeys= buffer.getInt(4); // # keys in the node
+				if (flag == 0) return startInd; // found the leaf node
+				int numKeys= buffer.getInt(4); // # of keys in the node
 				int prevKey= Integer.MAX_VALUE;
 				int curKey;
 				int pos= 0;
 				while (pos < numKeys) {
 					curKey= buffer.getInt(8 + 4 * pos); // actual keys in the node start from index 8
 					if (lowKey == prevKey || (lowKey > prevKey && lowKey < curKey)) { // Found the node where lowKey is
+																					  // or where it would be
 						break;
 					}
 					prevKey= curKey;
 					pos++ ;
 				}
-				startInd= buffer.getInt((2 + numKeys) * 4 + (pos * 4)); // metadata (2), keys (numKeys), pos to be
-																	    // skipped (pos), multiply by 4 to get bytes
+				startInd= buffer.getInt((2 + numKeys + pos) * 4); // metadata (2), keys (numKeys), pos to be
+																  // skipped (pos), multiply by 4 to get bytes
 
 			}
 
@@ -96,6 +100,14 @@ public class Deserializer {
 			e.printStackTrace();
 		}
 		return 0;
+	}
+
+	public int getNumLeaves() {
+		return numLeaves;
+	}
+
+	public int getStartLeaf() {
+		return startLeaf;
 	}
 
 }
--- DBPrac/src/physicalOperator/IndexScanOperator.java
+++ DBPrac/src/physicalOperator/IndexScanOperator.java
@@ -1,42 +1,117 @@
 package physicalOperator;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.List;
 
+import bpTree.Deserializer;
+import dataStructure.Catalog;
 import dataStructure.Tuple;
+import fileIO.BinaryTupleReader;
 
 /** This class implements index scan, a file scan that only retrieves a range (subset) of tuples
  * from a relation file using B+-tree indices */
 public class IndexScanOperator extends ScanOperator {
 
+	private String tableName;
+	private String colName;
+	private ArrayList<String> schema;
+	private FileInputStream fin;
 	private FileChannel fc;
+	private ByteBuffer buffer;
+	private BinaryTupleReader reader;
+	private boolean isClustered;
 	private int lo; // the low key, null means lack of a bound
 	private int hi; // the high key, null means lack of a bound
+	private int[] startRid;
+	private List<int[]> repo; // Rids
+	private int ptr; // point to the correct position in repo
 
 	/** @param tableName: the relation to scan
 	 * @param alias: the alias
-	 * @param index: the index to use
+	 * @param index: the index column to use
+	 * @param indexFile: the serialized B+- tree index file
 	 * @param isClustered: whether the index is clustered or not
 	 * @param lowkey: the lower bound of the range of the scan (inclusive)
-	 * @param highkey: the higher bound of the range of the scan (inclusive) */
-	public IndexScanOperator(String tableName, String alias, String index, boolean isClustered, int lowkey,
-		int highkey) {
+	 * @param highkey: the higher bound of the range of the scan (inclusive)
+	 * @throws IOException */
+	public IndexScanOperator(String tableName, String alias, String index, String indexFile, boolean isClustered,
+		int lowkey,
+		int highkey) throws IOException {
 		super(tableName, alias);
+		fin= new FileInputStream(indexFile);
+		fc= fin.getChannel();
+		buffer= ByteBuffer.allocate(4096);
+		this.tableName= tableName;
+		this.colName= index;
+		this.isClustered= isClustered;
 		this.lo= lowkey;
 		this.hi= highkey;
-
-	}
-
-	/** The initial root-to-leaf descent that gives the rid of the first matching tuple.
-	 * 
-	 * @param lowkey: the lower bound of the range of the scan (inclusive)
-	 * @param highkey: the higher bound of the range of the scan (inclusive) */
-	private void initialDescent() {
-
+		Deserializer dsl= new Deserializer(indexFile);
+		this.startRid= dsl.getRid(lo, hi);
+		Catalog catalog= Catalog.getInstance();
+		this.reader= new BinaryTupleReader(catalog.getDir(tableName)); // Dealing with alias?
+		this.schema= catalog.getSchema(catalog.getDir(tableName)); // Not sure
+		this.ptr= 0;
+		if (isClustered) {
+			reader.reset(startRid[0] * 4096 + startRid[1]);
+		} else {
+			int startLeaf= dsl.getStartLeaf();
+			while (true) {
+				buffer.clear();
+				fc.position(startLeaf * 4096);
+				fc.read(buffer);
+				assert (buffer.getInt(0) == 0);
+				int numElements= buffer.getInt(4);
+				int pos= 8;
+				for (int i= 0; i < numElements; i++ ) {
+					int key= buffer.getInt(pos);
+					int numRids= buffer.getInt(pos + 4);
+					if (key > hi) return;
+					if (key >= lo && key <= hi) {
+						for (int j= 0; j < numRids * 2; j+= 2) {
+							int[] rid= new int[] { buffer.getInt(pos + 8 + j * 4),
+									buffer.getInt(pos + 8 + (j + 1) * 4) };
+							repo.add(rid);
+						}
+					}
+					pos+= 8 + numRids * 8; // skip the first two metadata and numRids rids (each rid is 8 bytes)
+				}
+				startLeaf++ ;
+			}
+		}
 	}
 
 	@Override
 	public Tuple getNextTuple() {
-		return null;
+		if (isClustered) {
+			Tuple tp= reader.readNextTuple(); // assumes it's reading sorted data file
+			if (tp.getData(schema.indexOf(colName)) > hi) return null;
+			return tp;
+		}
+		if (ptr > repo.size()) return null;
+		reader.reset(repo.get(ptr)[0] * 4096 + repo.get(ptr)[1]);
+		ptr+= 1;
+		return reader.readNextTuple();
+
+	}
+
+	@Override
+	public void reset() {
+		if (isClustered) {
+			reader.reset(startRid[0] * 4096 + startRid[1]);
+		} else {
+			ptr= 0;
+		}
+
+	}
+
+	@Override
+	public String getTableName() {
+		return this.tableName;
 	}
 
 }
