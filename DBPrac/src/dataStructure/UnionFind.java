333806b97aa19a95c3cc1e61bd358da33a68b2e4
Ziwei Gu <zg48@cornell.edu>
2019-11-19 15:03:29 -0500

    add plan writer

--- DBPrac/src/dataStructure/UnionFind.java
+++ DBPrac/src/dataStructure/UnionFind.java
@@ -12,7 +12,7 @@ public class UnionFind {
 
 	/** Initialize class fields. */
 	public UnionFind() {
-		elements = new ArrayList<BlueBox>();
+		elements= new ArrayList<BlueBox>();
 	}
 
 	/** Find the bluebox that col is in. If no such box exists, create a new bluebox containing col.
@@ -20,35 +20,35 @@ public class UnionFind {
 	 * @param String col: the name of the column to look for
 	 * @return a bluebox whose attribute list contains col */
 	public BlueBox find(String col) {
-		for (BlueBox bb: elements) {
-			if (bb.getAttr().contains(col)) {
-				return bb;
-			}
+		for (BlueBox bb : elements) {
+			if (bb.getAttr().contains(col)) { return bb; }
 		}
-		List<String> attributes = new ArrayList<String>();
+		List<String> attributes= new ArrayList<String>();
 		attributes.add(col);
 		return new BlueBox(attributes);
 	}
 
-	/**
+	public List<BlueBox> getBlueBoxes() {
+		return elements;
+	}
+
+	/** Example: WHERE S.A = S.B AND S.B = S.C AND S.D = 15 Return: <[S.A, S.B, S.C]: [Null, Null]>,
+	 * <[S.D]: [15,15]>
 	 * 
-	 * Example: WHERE S.A = S.B AND S.B = S.C AND S.D = 15
-	 * Return: <[S.A, S.B, S.C]: [Null, Null]>, <[S.D]: [15,15]>
 	 * @param alias
-	 * @return
-	 */
+	 * @return */
 	public HashMap<List<String>, Integer[]> findSelect(String alias) {
-		HashMap<List<String>, Integer[]> attrTable = new HashMap<List<String>, Integer[]>();
-		List<String> attrs = new ArrayList<String>();
+		HashMap<List<String>, Integer[]> attrTable= new HashMap<List<String>, Integer[]>();
+		List<String> attrs= new ArrayList<String>();
 
 		// Loop through each bluebox stored
-		for(BlueBox bb: elements) {
-			List<String> attributes = bb.getAttr();
+		for (BlueBox bb : elements) {
+			List<String> attributes= bb.getAttr();
 
 			// Loop through the attributes list and try to find the attributes starting with alias
-			for (int i = 0; i < attributes.size(); ++i) {
-				String attr = attributes.get(i);
-				String tableName = attr.split("\\.")[0];
+			for (int i= 0; i < attributes.size(); ++i) {
+				String attr= attributes.get(i);
+				String tableName= attr.split("\\.")[0];
 				if (tableName.equals(alias)) {
 					attrs.add(attr);
 				}
@@ -57,54 +57,52 @@ public class UnionFind {
 			// if the attributes in this bluebox are not empty, add the arraylist into the hashmap
 			if (!attrs.isEmpty()) {
 				attrTable.put(attrs, new Integer[] { bb.getLower(), bb.getUpper() });
-				attrs = new ArrayList<String>();
+				attrs= new ArrayList<String>();
 			}
 		}
 		return attrTable;
 	}
 
-	/**
-	 * Example: " R.a=R.h=B.C  AND  S.d=B.e "    prevTables:[R,S]  topMostTable: [B]
-	 * Return:  [ ["R.a","R.h" , "B.c"], ["S.d" , "B.e"] ]
+	/** Example: " R.a=R.h=B.C AND S.d=B.e " prevTables:[R,S] topMostTable: [B] Return: [ ["R.a","R.h" ,
+	 * "B.c"], ["S.d" , "B.e"] ]
 	 * 
 	 * @param tableList
 	 * @param table2
-	 * @return
-	 */
+	 * @return */
 	public List<ArrayList<String>> findJoin(List<String> tableList, String table2) {
-		List<ArrayList<String>> joinCondition = new ArrayList<ArrayList<String>>();
+		List<ArrayList<String>> joinCondition= new ArrayList<ArrayList<String>>();
 
-		for(BlueBox bb: elements) {
-			List<String> attributes = bb.getAttr();
+		for (BlueBox bb : elements) {
+			List<String> attributes= bb.getAttr();
 
 			// Loop through the attributes list and try to find the attributes starting with alias
-			boolean found1 = false;
-			boolean found2 = false;
+			boolean found1= false;
+			boolean found2= false;
 
-			ArrayList<Integer> table1Indexes = new ArrayList<Integer>();
-			ArrayList<Integer> table2Indexes = new ArrayList<Integer>();
+			ArrayList<Integer> table1Indexes= new ArrayList<Integer>();
+			ArrayList<Integer> table2Indexes= new ArrayList<Integer>();
 
-			for (int i = 0; i < attributes.size(); ++i) {
-				String attr = attributes.get(i);
-				String tableName = attr.split("\\.")[0];
+			for (int i= 0; i < attributes.size(); ++i) {
+				String attr= attributes.get(i);
+				String tableName= attr.split("\\.")[0];
 				if (tableList.contains(tableName)) {
-					found1 = true;
+					found1= true;
 					table1Indexes.add(i);
 				}
 				if (tableName.equals(table2)) {
-					found2 = true;
+					found2= true;
 					table2Indexes.add(i);
 				}
 			}
 
 			// If there are attributes that match both table1 and table2, add them to a new arraylist
 			if (found1 && found2) {
-				ArrayList<String> attrList = new ArrayList<String>();
-				for (Integer index: table1Indexes) {
+				ArrayList<String> attrList= new ArrayList<String>();
+				for (Integer index : table1Indexes) {
 					attrList.add(attributes.get(index));
 				}
 
-				for (Integer index: table2Indexes) {
+				for (Integer index : table2Indexes) {
 					attrList.add(attributes.get(index));
 				}
 				joinCondition.add(attrList);
@@ -114,51 +112,49 @@ public class UnionFind {
 		return joinCondition;
 	}
 
-	/**
+	/** Example: " R.a=R.h=B.C AND S.d=B.e " prevTables:[R,S] topMostTable: [B] Return: [[R.a, B.c],
+	 * [S.d, B.e]]
 	 * 
-	 * Example: " R.a=R.h=B.C  AND  S.d=B.e "    prevTables:[R,S]  topMostTable: [B]
-	 * Return: [[R.a, B.c], [S.d, B.e]]
 	 * @param tableLeftList
 	 * @param table2
-	 * @return
-	 */
+	 * @return */
 	public List<ArrayList<String>> findJoinInBox(List<String> tableLeftList, String table2) {
-		List<ArrayList<String>> joinCondition = new ArrayList<ArrayList<String>>();
+		List<ArrayList<String>> joinCondition= new ArrayList<ArrayList<String>>();
 
-		for(BlueBox bb: elements) {
+		for (BlueBox bb : elements) {
 			// check if equality, both lower and upper will be set
 			if (bb.getEqual() != null) {
 				continue;
 			}
 
-			List<String> attributes = bb.getAttr();
+			List<String> attributes= bb.getAttr();
 
 			// Loop through the attributes list and try to find the attributes starting with alias
-			boolean foundLeftList = false;
-			boolean foundTableRight = false;
-			int leftListIndex = -1;
-			int rightTableIndex = -1;
+			boolean foundLeftList= false;
+			boolean foundTableRight= false;
+			int leftListIndex= -1;
+			int rightTableIndex= -1;
 
-			for (int i = 0; i < attributes.size(); ++i) {
+			for (int i= 0; i < attributes.size(); ++i) {
 				if (foundLeftList && foundTableRight) {
 					break;
 				}
-				
-				String attr = attributes.get(i);
-				String tableName = attr.split("\\.")[0];
+
+				String attr= attributes.get(i);
+				String tableName= attr.split("\\.")[0];
 				if (tableLeftList.contains(tableName)) {
-					foundLeftList = true;
-					leftListIndex = i;
+					foundLeftList= true;
+					leftListIndex= i;
 				}
 				if (tableName.equals(table2)) {
-					foundTableRight = true;
-					rightTableIndex = i;
+					foundTableRight= true;
+					rightTableIndex= i;
 				}
 			}
 
 			// If there are attributes that match both table1 and table2, add them to a new arraylist
 			if (foundLeftList && foundTableRight) {
-				ArrayList<String> attrList = new ArrayList<String>();
+				ArrayList<String> attrList= new ArrayList<String>();
 				attrList.add(attributes.get(leftListIndex));
 				attrList.add(attributes.get(rightTableIndex));
 
@@ -173,9 +169,9 @@ public class UnionFind {
 	 * @param BlueBox b1, BlueBox b2: two blue boxes to be merged */
 	public void merge(BlueBox b1, BlueBox b2) {
 		// Do we need to check if b1 and b2 are the same blue box
-		List<String> attributes = new ArrayList<String>(b1.getAttr());
+		List<String> attributes= new ArrayList<String>(b1.getAttr());
 		attributes.addAll(b2.getAttr());
-		BlueBox merged = new BlueBox(attributes);
+		BlueBox merged= new BlueBox(attributes);
 
 		// Keep blue box constraints true
 		// low
