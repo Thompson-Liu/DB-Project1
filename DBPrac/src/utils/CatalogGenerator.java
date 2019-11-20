c499ca34c2a6629664d0d0a975e6e035f8f7f161
Mingzhao <ml2579@cornell.edu>
2019-11-20 10:14:09 -0500

    merge

--- DBPrac/src/utils/CatalogGenerator.java
+++ DBPrac/src/utils/CatalogGenerator.java
@@ -1,27 +1,24 @@
 package utils;
 
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileOutputStream;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
 import java.util.ArrayList;
-
 import dataStructure.Catalog;
 import dataStructure.Tuple;
 import fileIO.BinaryTupleReader;
 
 public class CatalogGenerator {
-	
+
 	String dbDir; 
-	
+
 	public CatalogGenerator(String dbDir) {
 		this.dbDir = dbDir;
 	}
-	
+
 	/** Construct catalog from directory
 	 * 
 	 * @param directory directory to find the schema file
@@ -29,26 +26,27 @@ public class CatalogGenerator {
 	public void createCatalog() {
 		Catalog cat= Catalog.getInstance();
 		try {
-			FileReader schemafw= new FileReader(dbDir + "/schema.txt");
+			String current = new java.io.File( "." ).getCanonicalPath();
+			File f = new File(current + "/samples/input/db/schema.txt");
+			FileReader schemafw= new FileReader(f);
 			BufferedReader readSchema= new BufferedReader(schemafw);
-			String line;
 			BinaryTupleReader tableread;
 			//write to file
-			BufferedWriter writer= new BufferedWriter(new FileWriter(dbDir +"/stats.txt"));
+			BufferedWriter writer= new BufferedWriter(new FileWriter(current + "/samples/input/db/stats.txt"));
 			String nextLine= readSchema.readLine();
 			while ((nextLine= readSchema.readLine()) != null && nextLine !="") {
 				String[] schemaLine= nextLine.trim().split("\\s+");
 				String tableName= schemaLine[0];
-				String tableDir = dbDir + "/data/" + tableName;
+				String tableDir = current + "/samples/input/db/data/" + tableName;
 				cat.addDir(tableName,  tableDir);
 				writer.write(tableName+" ");
-				
+
 				ArrayList<String> schem= new ArrayList<String>();
 				for (int i= 1; i < schemaLine.length; i++ ) {
 					schem.add(schemaLine[i]);
 				}
 				cat.addSchema(tableName, schem);
-				
+
 				//read table
 				System.out.println(tableDir);
 				tableread = new BinaryTupleReader(tableDir);
@@ -76,8 +74,8 @@ public class CatalogGenerator {
 				writer.newLine();
 				tableread.close();	
 			}
-			
-			
+
+
 			writer.close();
 			readSchema.close();
 		} catch (IOException e) {
